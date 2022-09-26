0b386a937b2b1e07a7603625eb5f4156e1c345e2
向阳 <xiangqian_liu@163.com>
2022-09-26 17:29:51 +0800

    feat: 文件切片上传

---
+++ packages/network/useSliceUpload.ts
@@ -0,0 +1,15 @@
+/**
+ * 文件切片上传
+ * @param file 文件对象
+ * @param options 选项
+ */
+
+interface IOptions {
+  /**
+   * 切片大小
+   */
+  size: number
+}
+export function useSliceUpload (file: File, options: IOptions) {
+  // ***
+}
